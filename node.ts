3022f0505eb6a3880d5ae385ea51c4b3b6538e41
Adam <ajscott1@utas.edu.au>
2021-06-19 12:13:10 +1000

    Function stubs implemented

--- node.ts
+++ node.ts
@@ -5,35 +5,88 @@ let message : string = "aaa";
 console.log(message);
 
 
-class Node{
-    parents: List<Node>;
-    children: List<Node>;
+class NodeObj{
+    parents: []//list<NodeObj>;
+    children: []//list<NodeObj>;
     title: string;
-    path: string;//Or array?
+    path: []//list<String>;//Or array?
 
     constructor(){
 
     }
 
+    public getHeight(){
+        return this.path.length;
+    }
+
+    //TODO implement
+    public addChild(child: NodeObj): void{
+
+    }
+
+    //TODO implement
+    public deleteChild(child: NodeObj): void{
+
+    }
+
+    //TODO implement
+    public addParent(parent: NodeObj): void{
+
+    }
+
+    //TODO implement
+    public removeParent(parent: NodeObj): void{
+
+    }
+
+    //TODO implement
+    toString(node: NodeObj): string{
+
+        let vars : string = "";
 
 
-    /*
-        TODO
-        Add child
-        delete child
-        add new parent
-        remove parent
-        generate path
 
-    */
+        return vars;
+    }
+
 }
 
 class NodeTree{
 
-    root: Node;
+    root: NodeObj;//Tree representation
+    allNodes: Map<string, NodeObj>;//Store access for all nodes
 
-    constructor(newRoot: Node){
+    constructor(newRoot: NodeObj){
         this.root = newRoot;
+    }
+
+
+    GetNodeByTitle(title: string): NodeObj{
+        return this.allNodes.get(title);
+    }
+
+    addChild(path: [], newNode: NodeObj ): void{
+        let parentNode: NodeObj = this.allNodes.get(path[path.length - 1]);
+
+        parentNode.addChild(newNode);
+
+    }
+
+    deleteChild(nodeToDelete: NodeObj): void{
+
+    }
+
+    addParent(parentNode: NodeObj, newChild: NodeObj){
+
+    }
+
+    deleteParent(nodeToDelete: NodeObj): void{
+
+    }
+
+    treeToString(): string{
+
+        return "";
     }
 
     /*
