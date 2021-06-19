a746ed09de90015de7389259dc6d36e0574519a5
Adam <ajscott1@utas.edu.au>
2021-06-19 13:40:27 +1000

    Post order traversal implemented

--- node.ts
+++ node.ts
@@ -1,10 +1,3 @@
-
-
-
-let message : string = "aaa";
-console.log(message);
-
-
 class NodeObj{
     parents: NodeObj[]//list<NodeObj>;
     children: NodeObj[]//list<NodeObj>;
@@ -13,16 +6,20 @@ class NodeObj{
 
     constructor(parents: NodeObj[], title: string){
 
-        this.parents = parents;
         this.title = title;
         this.children = [];
+        this.parents = [];
+
+        for (var i = 0; i < parents.length; i++){
+            this.parents.push(parents[i]);
+        }
 
         
         if (parents.length != 0){
             this.path = new Array();
 
-            for (var i = 0; i < parents.length; i++){
-                this.path.push(parents[i].title);
+            for (var i = 0; i < parents[0].path.length; i++){
+                this.path.push(parents[0].path[i]);
             }
 
             this.path.push(title);
@@ -57,6 +54,18 @@ class NodeObj{
 
     }
 
+    public traverseChildren(parent: NodeObj): string{
+
+        var tabHeight = "\t";
+
+        for (var i = 0; i < parent.children.length; i++){
+            console.log(tabHeight.repeat(parent.getHeight())+ "v---> "  + this.traverseChildren(parent.children[i]));
+        }
+
+        return parent.title;
+    } 
+
+
     public toString(): string{
         return this.title + "- PATH:**" + this.path + "**, parent count: " + this.parents.length + ", children count: " + this.children.length;
     }
@@ -68,7 +77,7 @@ class NodeTree{
     allNodes: Map<string, NodeObj>;//Store access for all nodes
 
     constructor(newRoot: NodeObj){
-        this.allNodes = new Map();
+        this.allNodes = new Map<string, NodeObj>();
 
 
         this.root = newRoot;
@@ -100,8 +109,8 @@ class NodeTree{
     }
 
     treeToString(): string{
-
-        return "";
+        console.log("Printing tree for: " + tree.root.title);
+        return this.root.traverseChildren(this.root);
     }
 
     /*
@@ -114,17 +123,37 @@ class NodeTree{
 
     */
 
+
 }
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; // max & min both included 
+  }
 
 
 let tree: NodeTree = new NodeTree(new NodeObj([],"hello"));
 
-tree.addChild(new NodeObj(new Array(tree.root), "second node"));
-
-console.log(tree.root.toString());
-console.log(tree.allNodes["second node"].toString());
 
 
+for (var i = 0; i < 4; i++){
+    tree.addChild(new NodeObj(new Array(tree.root), i + " node out of 3(4)"));
+}
 
+for (var i = 10; i < 20; i++){
+    let allNodes = [...Object.values(tree.allNodes)];
+    console.log(i + " started........");
+    if (i == 19){
+        console.log(allNodes);
+    }
 
+    tree.addChild(new NodeObj(new Array(allNodes[getRandomIntInclusive(0, allNodes.length - 1)]), i + " random assigned node"));
+}
 
+//console.log(tree.root.toString());
+
+//console.log(tree.allNodes);
+
+console.log(tree.treeToString());
